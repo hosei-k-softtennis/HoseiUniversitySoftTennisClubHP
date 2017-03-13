738f315f8a567e9a1ed4584f332c3da77ae7d682
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-03-13 15:05:05 +0900

    update files

--- common/js/fullcalendar-2.3.1/fullcalendar.input.information.js
+++ common/js/fullcalendar-2.3.1/fullcalendar.input.information.js
@@ -1,13 +1,15 @@
 jQuery.noConflict();
-    (function($) {
-      $(document).ready(function () {
+(function($) {
+    $(document).ready(function () {
 	$('#calendar').fullCalendar({
 
-	  googleCalendarApiKey: 'AIzaSyB7M5IyWkvJOQsbhuxGbXQmTMZSE50YrhE',
-	  
-	  events:{
-	    googleCalendarId: 'hoseisofttenniskoganei@gmail.com'
-	  }
+	    // googleCalendarApiKey: 'AIzaSyB7M5IyWkvJOQsbhuxGbXQmTMZSE50YrhE',
+	    googleCalenderApiKey: 'AIzaSyAvqtb4BwbRRgU66-8DUV5MFMGweMqYcNk',
+	    
+	    events:{
+		// googleCalendarId: 'hoseisofttenniskoganei@gmail.com'
+		googleCalenderId: 'h.kotairen.softtennis@gmail.com'
+	    }
 	});
-      });
-    })(jQuery);
+    });
+})(jQuery);
